8f428508743f73d4ecc1cd3b91c4097ae53c5d7f
Goobrr <ggoobbrr@gmail.com>
2023-02-09 08:55:30 +0700

    Setup

---
+++ src/cell/CellStorage.java
@@ -0,0 +1,16 @@
+package cell;
+
+import mindustry.mod.*;
+
+public class CellStorage extends Mod{
+
+    public CellStorage(){
+
+    }
+
+    @Override
+    public void loadContent(){
+
+    }
+
+}
